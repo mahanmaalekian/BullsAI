bc1a42f871a53f0ec8cb817750669c78ffef4af3
Mahan Maalekian <maalekianmahan@gmail.com>
2025-01-25 23:15:12 -0500

    bane

--- pages/graphing.py
+++ pages/graphing.py
@@ -1,12 +1,13 @@
 # for graphs
 from dash import Dash, dcc, html, Input, Output, register_page, callback
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 
 # Load sample stock data
 df = pd.read_csv(r'data\trade_data_A_period_1.csv')
-sampled_df = df.sample(n=100, random_state=42)
-sampled_df = sampled_df.sort_values(by='timestamp')
+#sampled_df = df.sample(n=100, random_state=42)
+sampled_df = df.sort_values(by='timestamp')
 sampled_df["timestamp"] = pd.to_datetime(sampled_df["timestamp"])
 #print(sampled_df)
 df = pd.read_csv("data/hourly_stock_data.csv")
@@ -22,35 +23,26 @@ layout = html.Div(
             html.Img(src="/assets/logo.png"),
         ]),
         dcc.Dropdown(
-            id="ticker",
+            id="stock-dropdown",
             className="dropdown",
             options=[{"label": stock, "value": stock} for stock in ["AAPL", "GOOG", "MCSFT"]],
             value="AAPL",
             clearable=False,
         ),]),
         html.H4("Stock Price Analysis"),
-        html.P("Select Stock:"),
-        dcc.RadioItems(options=["AAPL", "GOOG", "MFT"], value='AAPL', id='ticker'),
-        dcc.RadioItems(options=["price", "volume"], value='price', id='feature'),
-
-        html.P("Select Start Time Period:"),
-        dcc.Dropdown(
-            id="time-period",
-            options=[
-                {"label": "5 Seconds", "value": "5S"},
-                {"label": "30 Seconds", "value": "30S"},
-                {"label": "1 Minute", "value": "1M"},
-                {"label": "3 Minutes", "value": "3M"},
-                {"label": "6 Minutes", "value": "6M"},
-                {"label": "10 Minutes", "value": "10M"},
-            ],
-            value="1M",
-            clearable=False,
-        ),
+        html.Div(id="time-periods", children=[
+            html.Div([html.P("Past"), 
+            dcc.RadioItems(options=["5S", "30S", "1M", "3M", "6M", "10M"], value='1M', id='time-period-past', inline=True),
+                      ]),
+            html.Div([html.P("Future"),
+            dcc.RadioItems(options=["5S", "30S", "1M", "3M", "6M", "10M"], value='1M', id='time-period-future', inline=True),
+        ]),]),
         dcc.Graph(id="time-series-chart"),
+        html.H4("Volume Analysis"),
+        dcc.Graph(id="volume-chart"),
 
-    ]
-)
+    ]),
+
 
 def filter_data_by_period_end(df, period):
     """Filters the data based on the selected time period."""
@@ -85,14 +77,14 @@ def filter_data_by_period_start(df, period):
     print("end date", end_date)
     start_date = end_date - period_to_time(period)
 
-    return df[df["timestamp"] >= start_date - pd.Timedelta(seconds=30) ]
+    return df[df["timestamp"] >= start_date - pd.Timedelta(seconds=5) ]
 
 
 @callback(
     Output("time-series-chart", "figure"),
-    [Input("feature", "value"), Input("time-period", "value")],
+    [Input("time-period-past", "value")],
 )
-def update_time_series(feature, period):
+def update_time_series(period):
     filtered_df = filter_data_by_period_start(sampled_df, period)
     if filtered_df.empty:
         print(f"Filtered dataframe is empty for period: {period}")
@@ -102,13 +94,54 @@ def update_time_series(feature, period):
     time_delta = period_to_time(period)
     print(f"Updating figure with {len(filtered_df)} points")
 
-    fig = px.line(filtered_df, x="timestamp", y=feature, title=f"{feature} Stock Price")
+    fig = px.line(filtered_df, x="timestamp", y="price", title=f"Stock Price")
     fig.update_layout(
         xaxis=dict(
             range=[current_date - time_delta, current_date],
             title="Timestamp"
         ),
-        yaxis=dict(title=feature.capitalize()),
+        yaxis=dict(title="Price"),
+        template="plotly_white"
+    )
+    return fig
+
+@callback(
+    Output("volume-chart", "figure"),
+    [Input("time-period-past", "value")],
+)
+def update_volume_chart(period):
+    filtered_df = filter_data_by_period_start(sampled_df, period)
+    if filtered_df.empty:
+        print(f"Filtered dataframe is empty for period: {period}")
+        return px.histogram(title="No data available for the selected period")
+
+    current_date = filtered_df["timestamp"].max()
+    time_delta = period_to_time(period)
+    print(f"Updating figure with {len(filtered_df)} points")
+
+    filtered_df.set_index("timestamp", inplace=True)
+    filtered_df["price_change"] = filtered_df["price"].diff()
+
+    aggregated_df = filtered_df.resample(time_delta/50).sum().reset_index()
+    aggregated_df["price_change"] = aggregated_df["price"].diff()
+    aggregated_df["color"] = aggregated_df["price_change"].diff().apply(lambda x: "green" if x > 0 else "red")
+
+
+    fig = px.bar(
+    aggregated_df,
+    x="timestamp",
+    y="volume",
+    color="color",  # Use the 'color' column for the bar colors
+    title="Stock Volume",
+    color_discrete_map={"green": "green", "red": "red"}  # Map colors to 'green' and 'red'
+    )
+    #fig = px.histogram(filtered_df, x="timestamp", y="volume", title=f"Stock Volume")
+    fig.update_layout(
+        xaxis=dict(
+            range=[current_date - time_delta, current_date],
+            title="Timestamp"
+        ),
+        yaxis=dict(title="Volume"),
         template="plotly_white"
     )
     return fig
